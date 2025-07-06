b393e8953818a38ec87e13e387e83f0757d6d41f
Nurhat Baydağ <baydagnurhat31@gmail.com>
2025-07-06 21:04:03 +0300

    bazı eklemeler yapıldı

---
+++ ViewComponents/ChatBoxViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using YoneticiOtomasyonu.Models;
+namespace YoneticiOtomasyonu.ViewComponents
+{
+    
+
+    public class ChatBoxViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChatBoxViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var profilePic = user?.ProfileImageUrl ?? "/images/default-profile.png";
+            ViewData["ProfilePic"] = profilePic;
+            ViewData["UserName"] = user?.UserName ?? "Misafir";
+
+            // Eğer chat kutusuna başlangıçta ihtiyaç duyulan başka veriler varsa burada hazırla
+
+            return View();
+        }
+    }
+
+}
